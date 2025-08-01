e32824c9daf924dcf3ce78b15493eb10755f4602
David Coronel <davecoronel@gmail.com>
2025-08-01 16:53:22 -0400

    added acm multicluster observability

---
+++ 05-acm/01-open-cluster-management-observability-namespace.sh
@@ -0,0 +1,1 @@
+oc create namespace open-cluster-management-observability
---
+++ 05-acm/02-secret.sh
@@ -0,0 +1,6 @@
+DOCKER_CONFIG_JSON=`oc extract secret/pull-secret -n openshift-config --to=-`
+
+oc create secret generic multiclusterhub-operator-pull-secret \
+    -n open-cluster-management-observability \
+    --from-literal=.dockerconfigjson="$DOCKER_CONFIG_JSON" \
+    --type=kubernetes.io/dockerconfigjson
